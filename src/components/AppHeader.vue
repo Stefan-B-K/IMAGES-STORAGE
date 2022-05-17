83d90757ff51b0ad0c0c2a4cdf2a949a258b6611
iStef <stefan_b_k@icloud.com>
2022-05-17 22:26:48 +0300

    Style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Image Storage</div>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item">Image Storage</a>
+    <div class="right menu">
+      <a class="ui item"> Logout </a>
+    </div>
+  </div>
 </template>
 
 <script>
