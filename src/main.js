0885dcfc458f75790a60154d5c1a27a1fff1496c
iStef <stefan_b_k@icloud.com>
2022-05-17 19:27:08 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+import Vue from 'vue'
+import App from './App'
 
-createApp(App).mount('#app')
+
+// createApp(App).mount('#app')
+new Vue ({
+  render: h => h(App)
+}).$mount('#app')
